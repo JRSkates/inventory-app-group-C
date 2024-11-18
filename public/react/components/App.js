feecbb9e18b96ed909c92483639a22cccb5e1d78
JRSkates <skatesproduction@gmail.com>
2024-11-18 12:48:00 +0000

    GET all items front end

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -6,14 +6,32 @@ import apiURL from "../api";
 function App() {
   const [items, setItems] = useState([]);
 
+  async function fetchItems() {
+    const response = await fetch(`${apiURL}/items`)
+    const data = await response.json();
+    setItems(data);
+  }
+
   useEffect(() => {
-    // Fetch the items
+    fetchItems();
   }, []);
 
   return (
     <>
       <h1>Inventory App</h1>
-      {/* Render the items */}
+      <ul>
+        {items.map((item) => (
+          <div>
+            <li key={item.id}>
+              <h3>{item.name}</h3>
+              <p>{item.description}</p>
+              <p>Price: £{item.price}</p>
+              <p>Category: {item.category}</p>
+              <img src={item.image} width={100} alt={item.name}></img>
+            </li>
+          </div>
+        ))}
+      </ul>
     </>
   );
 }
