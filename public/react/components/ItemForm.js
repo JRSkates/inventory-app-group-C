51a597ec6d5a02d8ceb7bc789587aff02fb03392
JRSkates <skatesproduction@gmail.com>
2024-11-22 14:08:45 +0000

    css linked file

--- public/react/components/ItemForm.js
+++ public/react/components/ItemForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './ItemForm.css'; 
+import './itemForm.css'; 
 
 const ItemForm = ({ onFormSubmit, onBack }) => {
     const [formData, setFormData] = useState({
